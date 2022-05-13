581c80ede7a6135dc68ec0294e2f531bdc3eb865
Jolan LARGETEAU <jolanlargeteau@gmail.com>
2022-05-13 21:34:25 +0200

    Add log function

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ URL = "https://qbittorrent/api/v2/"
 SID = "" # SID can be found in your qBittorrent cookies
 CONDITIONS = [condition.Condition("magnet_uri", "nyaa", 0), condition.Condition("default", "", 60)]
 
-
 COOKIES = {"SID": SID}
 
 if (URL[-1] != "/"):
     URL += "/"
+
+def log(level, message):
+    print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [" + str(level) + "] " + str(message))
 
 def check_conditions(torrent_data):
     if torrent_data["progress"] == 1:
@@ -21,7 +23,7 @@ def check_conditions(torrent_data):
                 return CONDITION.days
     return None
 
-print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [INFO] Job start !")
+log("INFO", "Job start !")
 
 response = requests.get(URL + "sync/maindata", cookies = COOKIES)
 torrents = response.json()["torrents"]
@@ -33,9 +35,9 @@ for torrent in torrents:
         added_on = datetime.fromtimestamp(torrent_data["added_on"])
         days_delta = timedelta(days=days)
         if added_on + days_delta < datetime.now():
-            print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [INFO] DELETE TORRENT " + torrent_data["name"])
+            log("INFO", "DELETE TORRENT " + torrent_data["name"])
             req_data = {"hashes": torrent, "deleteFiles": "true"}
             requests.post(URL + "torrents/delete", data=req_data, cookies=COOKIES)
 
             
-print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [INFO] Job end !")
+log("INFO", "Job end !")
