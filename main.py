1ba67c11350257831d4449dffe8694674b864ad2
Jolan LARGETEAU <jolanlargeteau@gmail.com>
2022-05-13 21:26:05 +0200

    Now, you can choose duration of torrent with conditions
    Signed-off-by: Jolan LARGETEAU <jolanlargeteau@gmail.com>

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-import requests
+from email.policy import default
+import requests, condition
 from datetime import datetime, timedelta
 
 URL = "https://qbittorrent/api/v2/"
 SID = "" # SID can be found in your qBittorrent cookies
-WEEKS = 2
+CONDITIONS = [condition.Condition("magnet_uri", "nyaa", 0), condition.Condition("default", "", 60)]
+
 
 COOKIES = {"SID": SID}
 
 if (URL[-1] != "/"):
     URL += "/"
 
-response = requests.get(URL + "sync/maindata", cookies = COOKIES)
+def check_conditions(torrent_data):
+    if torrent_data["progress"] == 1:
+        for CONDITION in CONDITIONS:
+            if CONDITION.id == "default":
+                return CONDITION.days
+            elif CONDITION.included_value in torrent_data[CONDITION.id]:
+                return CONDITION.days
+    return None
 
+print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [INFO] Job start !")
+
+response = requests.get(URL + "sync/maindata", cookies = COOKIES)
 torrents = response.json()["torrents"]
 
 for torrent in torrents:
     torrent_data = torrents[torrent]
-    added_on = datetime.fromtimestamp(torrent_data["added_on"])
-    week_delta = timedelta(weeks=WEEKS)
-    if (added_on + week_delta < datetime.now()):
-        print(str(datetime.now()) + " --- [INFO] DELETE TORRENT " + torrent_data["name"])
-        req_data = {"hashes": torrent, "deleteFiles": "true"}
-        requests.post(URL + "torrents/delete", data=req_data, cookies=COOKIES)
+    days = check_conditions(torrent_data)
+    if days != None:
+        added_on = datetime.fromtimestamp(torrent_data["added_on"])
+        days_delta = timedelta(days=days)
+        if added_on + days_delta < datetime.now():
+            print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [INFO] DELETE TORRENT " + torrent_data["name"])
+            req_data = {"hashes": torrent, "deleteFiles": "true"}
+            requests.post(URL + "torrents/delete", data=req_data, cookies=COOKIES)
+
+            
+print(datetime.now().strftime('%d-%m-%Y, %H:%M:%S') + " --- [INFO] Job end !")
